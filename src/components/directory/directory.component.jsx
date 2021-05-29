df8607877b2c4fc071c0888039d7238aca440e90
Abdelmounaim-Azz <abdelmounaim.azziza@outlook.fr>
2021-05-29 16:03:45 +0100

    pass on size value

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -3,8 +3,7 @@ import MenuItem from "../../components/menu-item/menu-item.component";
 import './directory.styles.scss'
 const Directory =()=>(
   <div className="directory-menu">
-    {sections.map(({title,imageUrl,id})=>(<MenuItem title={title} key={id} imageUrl={imageUrl}/>)
-      
+    {sections.map(({title,imageUrl,id,size})=>(<MenuItem title={title} key={id} imageUrl={imageUrl} size={size}/>)
     )}
   </div>
 )
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import './menu-item.styles.scss'
-const MenuItem=({title,imageUrl})=>(
-  <div style={{backgroundImage:`url(${imageUrl})`}} className="menu-item">
+const MenuItem=({title,imageUrl,size})=>(
+  <div style={{backgroundImage:`url(${imageUrl})`}} className={`menu-item ${size}`}>
         <div className="content">
           <h1 className="title">{title}</h1>
           <span className="subtitle">SHOP NOW</span>
