4ad2d596b5bc15b33c683cb9023393e415511f37
Abdelmounaim-Azz <abdelmounaim.azziza@outlook.fr>
2021-05-30 11:08:11 +0100

    multiple components showing router fix

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from "react";
-
-import {Switch, Route} from "react-router-dom";
-
+import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
 import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
@@ -9,13 +7,18 @@ import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 function App() {
   return (
-    <div>
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route exact path="/shop" component={ShopPage} />
-      </Switch>
-      <HomePage />
-    </div>
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/">
+            <HomePage />
+          </Route>
+          <Route exact path="/shop">
+            <ShopPage />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
