d768d1747ee4eaa57fb43e70050cef39b1189069
Abdelmounaim-Azz <abdelmounaim.azziza@outlook.fr>
2021-05-30 13:09:31 +0100

    header split links

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -7,11 +7,11 @@ const Header=()=>(
       <Logo className="logo" />
     </Link>
     <div className="options">
-      <Link to="/shop">
+      <Link className="option" to="/shop">
         SHOP
       </Link>
-      <Link to="/contact">
-        SHOP
+      <Link className="option" to="/contact">
+        CONTACT
       </Link>
     </div>
   </div>
