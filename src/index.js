acf7942fa2675e855033cb3f220d22e50990ec83
Abdelmounaim-Azz <abdelmounaim.azziza@outlook.fr>
2021-05-30 11:01:40 +0100

    solve dependency issues and setup routing

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
 import {BrowserRouter} from "react-router-dom";
 
+import App from "./App";
+
 ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
   document.getElementById("root")
 );
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import './homepage.styles.scss'
 import Directory from '../../components/directory/directory.component';
 const HomePage=()=>(
