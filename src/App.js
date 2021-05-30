8f4ed1c297545ea780fc61e6bef4b46428b3d8ea
Abdelmounaim-Azz <abdelmounaim.azziza@outlook.fr>
2021-05-30 17:48:22 +0100

    get currentUser in our app component+unsubscribe

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 import './App.css';
+import { auth } from './firebase/firebase.utils';
 
 import HomePage from './pages/homepage/homepage.component';
 import Header from './components/header/header.component';
 import ShopPage from './pages/shop/shop.component';
 import Auth from './pages/auth/auth.component';
 function App() {
+  const [currentUser, setCurrentUser] = useState(null);
+  useEffect(() => {
+    let unsubscribedFromAuth = null;
+    unsubscribedFromAuth = auth.onAuthStateChanged((user) =>
+      setCurrentUser(user)
+    );
+    return () => {
+      unsubscribedFromAuth();
+    };
+  }, [currentUser]);
+
   return (
     <Router>
       <div>
