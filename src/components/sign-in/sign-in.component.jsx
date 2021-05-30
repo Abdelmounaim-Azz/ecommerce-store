39ad33e6701679b8a8a2260d08cb3044098a6c99
Abdelmounaim-Azz <abdelmounaim.azziza@outlook.fr>
2021-05-30 17:06:24 +0100

    google sign in

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,6 +2,7 @@ import './sign-in.styles.scss';
 import { useState } from 'react';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-btn/custom-btn.component';
+import { SignInWithGoogle } from '../../firebase/firebase.utils';
 const SignIn = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
@@ -36,6 +37,9 @@ const SignIn = () => {
           required
         />
         <CustomButton type="submit">Sign in For Free</CustomButton>
+        <CustomButton onClick={SignInWithGoogle}>
+          Sign in With Google
+        </CustomButton>
       </form>
     </div>
   );
