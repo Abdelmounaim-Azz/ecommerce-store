61e38a30a350e423fa12cc63816f092ec61faf15
Abdelmounaim-Azz <abdelmounaim.azziza@outlook.fr>
2021-06-06 09:01:28 +0100

    persist redux state across session:localStorage

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor } from './redux/store';
-import App from './Aprtp';
+import App from './App';
 ReactDOM.render(
   <Provider store={store}>
     <PersistGate persistor={persistor}>
