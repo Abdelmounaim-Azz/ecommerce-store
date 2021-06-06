a34dcd42f4c0e122c42eac1aa3d4c8d0008512e5
Abdelmounaim-Azz <abdelmounaim.azziza@outlook.fr>
2021-06-06 09:02:05 +0100

    persist redux state across session:localStorage

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,5 @@
 import { createStore, applyMiddleware } from 'redux';
-import { persistStore } from 'redux-store';
+import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
 
 import rootReducer from './root-reducer';
